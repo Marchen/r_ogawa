773a3c8d388221fb61ac13921b29993d5dfe4120
Michio Oguro <michio_oguro@hotmail.com>
2022-11-01 16:37:34 +0900

    test: Add tests for all_touching().

---
+++ tests/test__all_touching.r
@@ -0,0 +1,48 @@
+testthat::context("Test all_touching()")
+
+#------------------------------------------------------------------------------
+testthat::test_that(
+    "Test for two Qs", {
+        # Adjacent Qs.
+        testthat::expect_true(
+            ogawa::all_touching("A1a1", "A1a2", corner = TRUE)
+        )
+        testthat::expect_true(
+            ogawa::all_touching("A1a1", "A1a2", corner = FALSE)
+        )
+        # Adjacent at corner.
+        testthat::expect_true(
+            ogawa::all_touching("A1a1", "A1a4", corner = TRUE)
+        )
+        testthat::expect_false(
+            ogawa::all_touching("A1a1", "A1a4", corner = FALSE)
+        )
+        # Distant Qs.
+        testthat::expect_false(
+            ogawa::all_touching("A1a1", "A2a4", corner = TRUE)
+        )
+        testthat::expect_false(
+            ogawa::all_touching("A1a1", "A2a4", corner = FALSE)
+        )
+    }
+)
+
+#------------------------------------------------------------------------------
+testthat::test_that(
+    "Test for three Qs", {
+        # Adjacent Qs.
+        testthat::expect_true(
+            ogawa::all_touching("A1a1", "A1a2", "A1a3", corner = TRUE)
+        )
+        testthat::expect_false(
+            ogawa::all_touching("A1a1", "A1a2", "A1a3", corner = FALSE)
+        )
+        # Distant Qs.
+        testthat::expect_false(
+            ogawa::all_touching("A1a1", "A1a2", "A2a4", corner = TRUE)
+        )
+        testthat::expect_false(
+            ogawa::all_touching("A1a1", "A1a2", "A2a4", corner = FALSE)
+        )
+    }
+)
